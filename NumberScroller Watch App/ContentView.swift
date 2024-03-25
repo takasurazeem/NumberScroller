36f74e6ac611b6ff3e0b5278ba45d682ba3a0fe8
Takasur Azeem <takasurazeem@gmail.com>
2024-03-25 15:42:51 +0500

    Initial Commit

---
+++ NumberScroller Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NumberScroller Watch App
+//
+//  Created by Takasur Home on 25/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NumberScroller Watch App/NumberScrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberScrollerApp.swift
+//  NumberScroller Watch App
+//
+//  Created by Takasur Home on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NumberScroller_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
